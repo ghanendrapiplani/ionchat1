332b16573585a7e58920eb1c62aae622943a03fb
ghanendra <ghanendrapiplani@gmail.com>
2018-06-20 09:25:35 +0530

    Update app.firebaseconfig.ts

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,8 +1,8 @@
 export var config = {
-    apiKey: "AIzaSyAg3nGkJJ0L4RDYZaY7hBg5_bJ2KpQVRFI",
+    apiKey: "",
     authDomain: "ionic3chat-ba936.firebaseapp.com",
     databaseURL: "https://ionic3chat-ba936.firebaseio.com",
     projectId: "ionic3chat-ba936",
     storageBucket: "ionic3chat-ba936.appspot.com",
-    messagingSenderId: "717409372659"
+    messagingSenderId: ""
   };
